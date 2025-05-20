22b7f9883efe7023f6a8b12b23c21d7a10e5f4a1
HyeungJun <azaz3769@gmail.com>
2025-05-20 14:19:56 +0900

    commit

--- app/login.tsx
+++ app/login.tsx
@@ -32,7 +32,7 @@ const LoginScreen: React.FC = () => {
       return;
     }
     console.log(`[로그인 시도] User ID: ${userId}`);
-    const API_BASE_URL = 'http://220.67.0.96:8080'; // 실제 IP 주소로 변경하세요
+    const API_BASE_URL = 'http://192.168.149.1:8080'; // 실제 IP 주소로 변경하세요
     const LOGIN_API_URL = `${API_BASE_URL}/api/auth/login`;
 
     try {
