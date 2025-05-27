c1bbc2dd6035b8df68920d8b85f1c853d375f1ff
jeongsehun0102 <gnstpwjd03@naver.com>
2025-05-27 12:24:59 +0900

    Revert "Merge branch 'main' into sehun"
    This reverts commit 5cdae80f84e76377f407b59d304cd339704b44b0, reversing
    changes made to 9438a42e18200d4e611bec90bd94c2aa330007ac.

--- app/login.tsx
+++ app/login.tsx
@@ -18,8 +18,7 @@ import {
 import { useAuth } from '@/context/AuthContext'; //
 import AnimatedCharacter from '../components/AnimatedCharacter'; //
 // 🔽 API 상수 import 추가
-import { API_BASE_URL, API_ENDPOINTS } from '../constants/api';
- // './constants/api'로 경로 수정 (app 폴더 기준)
+import { API_BASE_URL, API_ENDPOINTS } from '../constants/api'; // './constants/api'로 경로 수정 (app 폴더 기준)
 
 const LoginScreen: React.FC = () => {
   const [userId, setUserId] = useState<string>('');
